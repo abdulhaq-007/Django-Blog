b6a2a71ec2389d310464e8d2b1effda901bc5df9
abdulhaq-007 <85011586+abdulhaq-007@users.noreply.github.com>
2022-01-11 13:13:42 -0800

    create models

--- blog/settings.py
+++ blog/settings.py
@@ -118,7 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+import os
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'staticfiles')) ,
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- main/models.py
+++ main/models.py
@@ -1,7 +1,27 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField("Name", max_length=100)
+    surname = models.CharField("Surname", max_length=100)
+    age = models.CharField("Age", max_length=50)
+    year = models.CharField("Year", max_length=30)
+    image = models.ImageField("Image", upload_to='images/')
+
+    def __str__(self):
+        return f"{self.name}"
+
 class Post(models.Model):
-    title = models.Charfield("Post nomi", max_length=150)
-    slug = models.Slugfield("*", max_length=150)
+    title = models.CharField("Post nomi", max_length=150)
+    slug = models.SlugField("*", max_length=150)
     image = models.ImageField(upload_to='')
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.pk)])
